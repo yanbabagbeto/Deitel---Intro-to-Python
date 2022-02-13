a172382fcfd9a383369a5d856518c0ddf3bf4b3d
Yan Babagbeto <baby3102@usherbrooke.ca>
2022-02-13 17:12:46 -0500

    added exercise 5.2 solution

---
+++ chapter5/exercise5_2.py
@@ -0,0 +1,6 @@
+# 5.2 (What’s Does This Code Do?) What does the following function do, based on the
+# sequence it receives as an argument?
+# def mystery(sequence):
+# return sequence == sorted(sequence)
+
+# This code will return a True or False. Normally it will always return False because comparing the 2 objects sequence and sorted sequence will always point to deferent adresses.
